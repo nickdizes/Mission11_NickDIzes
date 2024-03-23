2d323ad8dd3e5637e4ffa201bc1c701eb5eab7f5
nickdizes <nick.dizes18@gmail.com>
2024-03-22 21:50:03 -0600

    Commit2

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Mission11_NickDizes.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- obj/Debug/net8.0/Mission11_NickDizes.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mission11_NickDizes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_NickDizes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159fee4eae7aed54a96ffcd449bab810a65dbf13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_NickDizes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_NickDizes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
